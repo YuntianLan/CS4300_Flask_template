02a657111d7af9067dc968236a1893d9b26df948
Yifan <yj77@cornell.edu>
2019-04-28 20:11:25 -0400

    pass char1under to front end

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -21,22 +21,23 @@ def result():
 			fandoms.append(i)
 
 	cnames, mnames, quotes, urls, vecs, user_vec = matcher.match(query, fandoms)
+	cnames_under = [cname.replace(" ","_") for cname in cnames]
 	return render_template('result.html',
 		user_vec = user_vec,
 
-		char1=cnames[0], movie1=mnames[0], vec1=vecs[0],
+		char1=cnames[0], char1under=cnames_under[0], movie1=mnames[0], vec1=vecs[0],
 		quote1=quotes[0][0].split('\n'), saidby1=quotes[0][1], url1=urls[0],
 
-		char2=cnames[1], movie2=mnames[1], vec2=vecs[1],
+		char2=cnames[1], char2under=cnames_under[1], movie2=mnames[1], vec2=vecs[1],
 		quote2=quotes[1][0].split('\n'), saidby2=quotes[1][1], url2=urls[1],
 
-		char3=cnames[2], movie3=mnames[2], vec3=vecs[2],
+		char3=cnames[2], char3under=cnames_under[2], movie3=mnames[2], vec3=vecs[2],
 		quote3=quotes[2][0].split('\n'), saidby3=quotes[2][1], url3=urls[2],
 
-		char4=cnames[3], movie4=mnames[3], vec4=vecs[3],
+		char4=cnames[3], char4under=cnames_under[3], movie4=mnames[3], vec4=vecs[3],
 		quote4=quotes[3][0].split('\n'), saidby4=quotes[3][1], url4=urls[3],
 
-		char5=cnames[4], movie5=mnames[4], vec5=vecs[4],
+		char5=cnames[4], char5under=cnames_under[4], movie5=mnames[4], vec5=vecs[4],
 		quote5=quotes[4][0].split('\n'), saidby5=quotes[4][1], url5=urls[4],
 	)
 
